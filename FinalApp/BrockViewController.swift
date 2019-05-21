ffa7d56044ab03e8a5271a2da88f7f89ec9f418c
Ramon <ramon.fuentes20@d155.org>
2019-05-21 08:49:08 -0500

    Progress

---
+++ FinalApp/BrockViewController.swift
@@ -0,0 +1,33 @@
+//
+//  BrockViewController.swift
+//  FinalApp
+//
+//  Created by RAMON FUENTES on 5/21/19.
+//  Copyright © 2019 RAMON FUENTES. All rights reserved.
+//
+
+import UIKit
+
+class BrockViewController: UIViewController {
+
+    @IBOutlet weak var name: UITextField!
+    
+    @IBOutlet weak var weight: UITextField!
+    
+    @IBOutlet weak var regionals: UITextField!
+    
+    @IBOutlet weak var sectionals: UITextField!
+    
+    @IBOutlet weak var state: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func brockEdit(_ sender: Any) {
+        
+    }
+    
+}
